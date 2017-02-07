c5e97d39c0114a45d9b98a2359693233ca0540e8
Clark Xu <xu_chl@163.com>
2017-02-07 13:54:12 +0800

    base-syntax

---
+++ base-syntax/1.py
@@ -0,0 +1,13 @@
+##### 串联两个字符串 #####
+
+# In [1]: 'clark'+'xu'
+# Out[1]: 'clarkxu'
+
+# In [2]: 'clark'  'xu'
+# Out[2]: 'clarkxu'
+# In [3]: print('clark','xu')
+print('clark','xu')
+# In [4]: 'clark %s'%('xu')
+# Out[4]: 'clark xu'
+
+##### os模块导航文件系统 #####
